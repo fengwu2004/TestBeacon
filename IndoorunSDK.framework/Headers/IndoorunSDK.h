2417b62c856b7f36ac35b10b3eff55238a301a8a
晏立 <yanli@yellfun.com>
2017-01-14 19:41:19 +0800

    save

---
+++ IndoorunSDK.framework/Headers/IndoorunSDK.h
@@ -0,0 +1,22 @@
+//
+//  IndoorunSDK.h
+//  IndoorunSDK
+//
+//  Created by ky on 10/12/16.
+//  Copyright © 2016 yellfun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <IndoorunSDK/IDRBaseLocationServer.h>
+#import <IndoorunSDK/IDRLogManager.h>
+
+//! Project version number for IndoorunSDK.
+FOUNDATION_EXPORT double IndoorunSDKVersionNumber;
+
+//! Project version string for IndoorunSDK.
+FOUNDATION_EXPORT const unsigned char IndoorunSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IndoorunSDK/PublicHeader.h>
+
+
--- TestBeacon/ViewController.m
+++ TestBeacon/ViewController.m
@@ -7,22 +7,129 @@
 //
 
 #import "ViewController.h"
+#import <IndoorunSDK/IndoorunSDK.h>
 
-@interface ViewController ()
+@interface ViewController ()<IDRBaseLocationServerDelegate, UITextViewDelegate>
+
+@property (nonatomic) IDRBaseLocationServer *baseLocator;
+@property (nonatomic, copy) NSString *major;
+@property (nonatomic, copy) NSString *minor;
+@property (nonatomic, copy) NSString *beaconUUID;
+@property (nonatomic, copy) NSString *testCount;
+
+@property (nonatomic) IBOutlet UITextView *ibMajor;
+@property (nonatomic) IBOutlet UITextView *ibMinor;
+@property (nonatomic) IBOutlet UITextView *ibTestCount;
+@property (nonatomic) IBOutlet UILabel *ibUUID;
+@property (nonatomic) IBOutlet UIButton *ibStart;
+@property (nonatomic) IBOutlet UIButton *ibEnd;
 
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
+    
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.beaconUUID = @"FDA50693-A4E2-4FB1-AFCF-C6EB07647825";
+    
+    self.testCount = @"0";
+    
+    self.major = @"0";
+    
+    self.minor = @"0";
+    
+    [_ibUUID setText:self.beaconUUID];
+    
+    [_ibTestCount setText:self.testCount];
+    
+    [_ibMajor setText:self.major];
+    
+    [_ibMinor setText:self.minor];
 }
 
+- (void)startCollect {
+    
+    _major = _ibMajor.text;
+    
+    _minor = _ibMinor.text;
+    
+    _testCount = _ibTestCount.text;
+    
+    [_ibStart setEnabled:NO];
+    
+    if (!_baseLocator) {
+        
+        _baseLocator = [[IDRBaseLocationServer alloc] init];
+    }
+    
+    [[IDRLogManager sharedInstance] log1:@"开始记录"];
+    
+    [[IDRLogManager sharedInstance] log1:[NSString stringWithFormat:@"测试干扰数据源:%@", _ibTestCount.text]];
+    
+    [_baseLocator setBeaconUUID:@[_beaconUUID]];
+    
+    [_baseLocator setDelegate:self];
+    
+    [_baseLocator startUpdateBeacons];
+}
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)onSetCountOk:(id)sender {
+    
+    [_ibMajor resignFirstResponder];
+    
+    [_ibMinor resignFirstResponder];
+    
+    [_ibTestCount resignFirstResponder];
+}
+
+- (IBAction)onStart:(id)sender {
+    
+    [self startCollect];
+}
+
+- (IBAction)onEnd:(id)sender {
+    
+    [_baseLocator stopUpdateBeacons];
+    
+    _baseLocator = nil;
+    
+    [[IDRLogManager sharedInstance] log1:@"结束采集"];
+    
+    [[IDRLogManager sharedInstance] flush];
+    
+    [_ibStart setEnabled:YES];
+}
+
+- (void)textViewDidEndEditing:(UITextView *)textView {
+    
+    [_ibMinor resignFirstResponder];
+    
+    [_ibMinor resignFirstResponder];
+    
+    [_ibTestCount resignFirstResponder];
+}
+
+- (void)didGetRangeBeacons:(NSArray *)beacons {
+    
+    NSString *value = @"没有采集到";
+    
+    NSString *mm = [NSString stringWithFormat:@"major:%@minor:%@", _major, _minor];
+    
+    for (NSDictionary *data in beacons) {
+        
+        NSString *mac = [data objectForKey:@"mac"];
+        
+        NSString *rssi = [data objectForKey:@"rss"];
+        
+        if ([mac isEqualToString:mm]) {
+            
+            value = [rssi copy];
+        }
+    }
+    
+    [[IDRLogManager sharedInstance] log1:value];
 }
 
 
