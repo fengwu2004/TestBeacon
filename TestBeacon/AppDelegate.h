16c7f8c507571f2ec0d1a2fefb15847a256cbf8a
晏立 <yanli@yellfun.com>
2017-01-14 17:33:25 +0800

    Initial Commit

---
+++ TestBeacon/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestBeacon
+//
+//  Created by ky on 14/01/2017.
+//  Copyright © 2017 yellfun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestBeacon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestBeacon
+//
+//  Created by ky on 14/01/2017.
+//  Copyright © 2017 yellfun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
